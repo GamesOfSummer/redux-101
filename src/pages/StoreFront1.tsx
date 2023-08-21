5bf83083ea9b44fe483c570b837f33e7dd829a6c
Summer T <summer@summertime-development.com>
2023-08-21 13:50:15 -0500

    added buttons

--- src/pages/StoreFront1.tsx
+++ src/pages/StoreFront1.tsx
@@ -3,10 +3,14 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 function StoreFront1() {
+  function handleClick() {}
+
   return (
     <div>
       <h1>Store 1</h1>
       <Link href="/StoreFront2">Go to Store Front 2</Link>
+      <br />
+      <button onClick={handleClick}>Add Item 1</button>
     </div>
   );
 }
--- src/pages/StoreFront2.tsx
+++ src/pages/StoreFront2.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { useRouter } from "next/router";
 import Link from "next/link";
 
-export default function StoreFront2() {
+function StoreFront2() {
+  function handleClick() {}
+
   return (
     <div>
       <h1>Store 2</h1>
       <Link href="/StoreFront1">Go to Store Front 1</Link>
+      <br />
+      <button onClick={handleClick}>Add Item 2</button>
     </div>
   );
 }
+
+export default StoreFront2;
